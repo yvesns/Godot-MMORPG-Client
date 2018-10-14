3d39d388ba86ae48984833136dcb4c100c8c9f82
yvesns <yves.ns2@gmail.com>
2018-10-14 17:59:02 -0300

    Character creation

---
+++ Scenes/UI/CharacterCreation/CharacterCreation.gd
@@ -0,0 +1,12 @@
+extends TextureRect
+
+# TODO
+# 1. Add character preview
+# 2. Add stats preview
+# 3. Add character customization
+# 4. Add gui theme
+
+var button_group = ButtonGroup.new()
+
+func _ready():
+	pass
