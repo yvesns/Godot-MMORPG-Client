dc19417e429dc61e60f633031d54d7f6faf4e6df
yvesns <yves.ns2@gmail.com>
2018-10-15 18:02:39 -0300

    Character creation

--- Scenes/UI/CharacterCreation/CharacterCreation.gd
+++ Scenes/UI/CharacterCreation/CharacterCreation.gd
@@ -6,7 +6,31 @@ extends TextureRect
 # 3. Add character customization
 # 4. Add gui theme
 
-var button_group = ButtonGroup.new()
+var character_info = {
+	character_name = "",
+	character_race = "",
+	character_class =  "Common"
+}
+var race_button_group = ButtonGroup.new()
 
 func _ready():
-	pass
+	find_node("HumanCheckbox").group = race_button_group
+	find_node("VampireCheckbox").group = race_button_group
+
+func _on_LineEdit_text_changed(new_name):
+	character_info.character_name = new_name
+	
+func _on_HumanCheckbox_button_up():
+	character_info.character_race = "Human"
+
+func _on_VampireCheckbox_button_up():
+	character_info.character_race = "Vampire"
+
+func _on_Create_button_up():
+	if (character_info.character_name == "" ||
+	    character_info.character_race == ""):
+		return
+	
+	character_info.character_class += character_race
+	
+	Network.create_character(character_info)
