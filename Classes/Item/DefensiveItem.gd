f7c27021686f496fa6b047bd87eacf88c41049f8
yvesns <yves.ns2@gmail.com>
2018-12-13 18:02:46 -0200

    Item class structure

--- Classes/Item/DefensiveItem.gd
+++ Classes/Item/DefensiveItem.gd
@@ -5,3 +5,21 @@ var armour
 
 func _ready():
 	pass
+	
+func serialize():
+	return {
+		texture = self.texture,
+		state = self.state,
+		item_id = self.item_id,
+		item_name = self.item_name,
+		item_class = self.item_class,
+		item_type = self.item_type,
+		item_rarity = self.item_rarity,
+		item_options = JSON.print(self.item_options, "", true),
+		inventory_slot_width = self.inventory_slot_width,
+		inventory_slot_height = self.inventory_slot_height,
+		armour = self.armour
+	}
+	
+func deserialize(json_item):
+	pass
--- Classes/Item/Item.gd
+++ Classes/Item/Item.gd
@@ -24,6 +24,51 @@ func is_stackable():
 func get_texture():
 	return texture
 	
+func set_texture(texture):
+	self.texture = texture
+	
+func get_id():
+	return item_id
+	
+func set_id(item_id):
+	self.item_id = item_id
+	
+func get_name():
+	return item_name
+	
+func set_name(item_name):
+	self.item_name = item_name
+	
+func get_class():
+	return item_class
+	
+func set_class(item_class):
+	self.item_class = item_class
+	
+func get_type():
+	return item_type
+	
+func set_type(item_type):
+	self.item_type = item_type
+	
+func get_rarity():
+	return item_rarity
+	
+func set_rarity(item_rarity):
+	self.item_rarity = item_rarity
+	
+func get_inventory_width():
+	return inventory_slot_width
+	
+func set_inventory_width(inventory_slot_width):
+	self.inventory_slot_width = inventory_slot_width
+	
+func get_inventory_height():
+	return inventory_slot_height
+	
+func set_inventory_height(inventory_slot_height):
+	self.inventory_slot_height = inventory_slot_height
+	
 func get_width():
 	return Global.inventory_slot_size * inventory_slot_width
 	
@@ -32,9 +77,3 @@ func get_height():
 	
 func get_slot_count():
 	return inventory_slot_width * inventory_slot_height
-	
-func serialize():
-	return JSON.print(self, "", true)
-	
-func deserialize(json_item):
-	pass
